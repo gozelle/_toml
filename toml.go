8c82cdb3eb054871e50438da656e4fd49cf75d16
koyeo <koyeo@qq.com>
2021-07-06 10:38:37 +0800

    update toml

--- toml.go
+++ toml.go
@@ -3,6 +3,7 @@ package _toml
 import (
 	"bytes"
 	"github.com/BurntSushi/toml"
+	"io/ioutil"
 )
 
 func Unmarshal(p []byte, v interface{}) error {
@@ -11,6 +12,14 @@ func Unmarshal(p []byte, v interface{}) error {
 
 func UnmarshalText(p string, v interface{}) error {
 	return toml.Unmarshal([]byte(p), v)
+}
+
+func UnmarshalFile(path string, v interface{}) error {
+	a, err := ioutil.ReadFile(path)
+	if err != nil {
+		return nil
+	}
+	return Unmarshal(a, v)
 }
 
 func Marshal(v interface{}) ([]byte, error) {
@@ -23,9 +32,31 @@ func Marshal(v interface{}) ([]byte, error) {
 	return buffer.Bytes(), nil
 }
 
+func MarshalIdent(v interface{}, ident string) ([]byte, error) {
+	var buffer bytes.Buffer
+	e := toml.NewEncoder(&buffer)
+	e.Indent = ident
+	err := e.Encode(v)
+	if err != nil {
+		return nil, err
+	}
+	return buffer.Bytes(), nil
+}
+
 func MarshalText(v interface{}) (string, error) {
 	var buffer bytes.Buffer
 	e := toml.NewEncoder(&buffer)
+	err := e.Encode(v)
+	if err != nil {
+		return "", err
+	}
+	return buffer.String(), nil
+}
+
+func MarshalTextIdent(v interface{}, ident string) (string, error) {
+	var buffer bytes.Buffer
+	e := toml.NewEncoder(&buffer)
+	e.Indent = ident
 	err := e.Encode(v)
 	if err != nil {
 		return "", err
